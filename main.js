fa159c583e5b9417a7216634c41c9c82c83b92c4
TaiNguyen <tainguyen05082002@gmail.com>
2025-01-04 23:07:03 +0700

    First commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const tasks = [
+  {
+    title: 'Do homework',
+    completed: false,
+  },
+
+  {
+    title: 'Coding',
+    completed: true,
+  },
+
+  {
+    title: 'Reading',
+    completed: false,
+  },
+];
